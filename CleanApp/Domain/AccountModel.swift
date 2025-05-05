c3cfff8b3c8a8baa4bdae898c400d2ebd33be843
CarolinaPaulo <carolinadefpaulo@gmail.com>
2025-05-05 20:09:17 -0300

    add AddAccount use case

---
+++ CleanApp/Domain/AccountModel.swift
@@ -0,0 +1,15 @@
+//
+//  AccountModel.swift
+//  Domain
+//
+//  Created by Carolina Paulo on 05/05/25.
+//
+
+import Foundation
+
+struct AccountModel {
+    var id: String
+    var name: String
+    var email: String
+    var password: String
+}
